356819a105c79d4601932bdbaccf931a356d0633
Ilya Senchukov <senchukov02@gmail.com>
2021-03-30 17:01:51 +0300

    Example app add

---
+++ Example/ExampleApp/ExampleApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Ilya Senchukov on 30.03.2021.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionVIew: UICollectionView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
