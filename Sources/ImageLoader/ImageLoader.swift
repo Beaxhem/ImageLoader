0d3fb87a4452bc6d9e7daedcff65340cf391baee
Ilya Senchukov <senchukov02@gmail.com>
2021-03-30 17:00:14 +0300

    Example app added

--- Sources/ImageLoader/ImageLoader.swift
+++ Sources/ImageLoader/ImageLoader.swift
@@ -1,3 +1,78 @@
-struct ImageLoader {
-    var text = "Hello, World!"
+import UIKit
+import NetworkManagement
+
+class ImageLoader {
+
+    private let cache = ImageCache()
+
+    var networkManager: NetworkManager?
+
+    func loadImage(from url: URL, completion: @escaping (UIImage?) -> Void) {
+        if let image = cache.image(for: url) {
+            completion(image)
+            return
+        }
+
+        networkManager?.dataTask(url: url, completion: { [weak self] (res: Result<Data, NetworkError>) in
+            switch res {
+                case .failure(let error):
+                    print(error)
+                    completion(nil)
+                case .success(let data):
+                    guard let image = UIImage(data: data) else {
+                        completion(nil)
+                        return
+                    }
+                    self?.cache.insertImage(image: image, for: url)
+                    completion(image)
+            }
+        })
+    }
+}
+
+final class ImageCache {
+
+    private lazy var imageCache: NSCache<AnyObject, AnyObject> = {
+        let cache = NSCache<AnyObject, AnyObject>()
+
+        cache.countLimit = config.countLimit
+
+        return cache
+    }()
+
+    private let config: Config
+    private let lock = NSLock()
+
+    struct Config {
+
+        var countLimit: Int
+        let memoryLimit: Int
+
+        static let `default` = Config(countLimit: 100, memoryLimit: 1024 * 1024 * 100)
+    }
+
+    init(config: Config = Config.default) {
+        self.config = config
+    }
+}
+
+extension ImageCache {
+
+    func insertImage(image: UIImage, for url: URL) {
+        let decodedImage = image.decoded()
+
+        lock.lock(); defer { lock.unlock() }
+
+        imageCache.setObject(decodedImage, forKey: url as AnyObject)
+    }
+
+    func image(for url: URL) -> UIImage? {
+        lock.lock(); defer { lock.unlock() }
+
+        if let image = imageCache.object(forKey: url as AnyObject) {
+            return image as? UIImage
+        }
+
+        return nil
+    }
 }
---
+++ Sources/ImageLoader/UIImage+Decoded.swift
@@ -0,0 +1,30 @@
+//
+//  UIImage+Decoded.swift
+//  
+//
+//  Created by Ilya Senchukov on 29.03.2021.
+//
+
+import UIKit
+
+extension UIImage {
+
+    func decoded() -> UIImage {
+        guard let cgImage = cgImage else {
+            return self
+        }
+
+        let size = CGSize(width: cgImage.width, height: cgImage.height)
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+
+        let context = CGContext(data: nil, width: Int(size.width), height: Int(size.height), bitsPerComponent: 8, bytesPerRow: cgImage.bytesPerRow, space: colorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+
+        context?.draw(cgImage, in: CGRect(origin: .zero, size: size))
+
+        guard let decodedImage = context?.makeImage() else {
+            return self
+        }
+
+        return UIImage(cgImage: decodedImage)
+    }
+}
